c3a5026db6b505fbc679c514261d5f3cd389af8f
Valeev Azamat <azamat@mail.sao.ru>
2024-06-04 22:37:15 +0300

    main.py use constant variable values for setting the parameters (not use KEYBOARD values)

--- src/main.py
+++ src/main.py
@@ -41,13 +41,26 @@ def get_formatted_vacancies(vacancies_list):
 def user_interaction():
     is_online = False
 
+    # search_query = input("Введите поисковый запрос: ")
+    # top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+    # keyword_list = input("Введите ключевые слова для фильтрации вакансий: ").split()
+    # salary_min = input("Введите нижнюю планку зарплаты: ") # Пример: 100000
+    # salary_max = input("Введите верхнюю планку зарплаты: ") # Пример: 150000
+
+    search_query = "Python"
+    top_n = 5
+    # keyword_list = ["танц"]
+    keyword_list = ["Python", "Git"]
+    salary_min = 1
+    salary_max = 1e9
+
     # Создание экземпляра класса для работы с API сайтов с вакансиями
     filename = os.path.join("..", "data", "hh_python.json")
 #    filename = os.path.join("..", "data", "vacancies_example.json")
     hh = HeadHunterParser(filename)
     if is_online:
         # Получение вакансий с hh.ru в формате JSON
-        hh.load_vacancies("Python")
+        hh.load_vacancies(search_query)
         hh.save_to_file()
     else:
         hh.load_from_file()
@@ -55,16 +68,7 @@ def user_interaction():
     # Преобразование набора данных из JSON в список объектов
     vacancies_list = hh.parse_and_verify()
 
-    # search_query = input("Введите поисковый запрос: ")
-    # top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-    # keyword_list = input("Введите ключевые слова для фильтрации вакансий: ").split()
-    # salary_range = input("Введите диапазон зарплат: ") # Пример: 100000 - 150000
 
-    top_n = 5
-    keyword_list = ["танц"]
-    keyword_list = ["Python", "Git"]
-    salary_min = 1
-    salary_max = 1e9
 
     filtered_vacancies = filter_vacancies(vacancies_list, keyword_list)
     ranged_vacancies = get_vacancies_by_salary(filtered_vacancies, salary_min, salary_max)
